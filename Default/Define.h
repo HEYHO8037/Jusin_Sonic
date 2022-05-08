644416b843842e60327173d121ef753dcfc68381
HEYHO\haeho <haeho8037@gmail.com>
2022-05-08 14:52:34 +0900

    add numeral value for physics

--- Default/Define.h
+++ Default/Define.h
@@ -19,5 +19,19 @@
 #define		TILEX		30
 #define		TILEY		20
 
+#define ACC 0.46875 //가속력
+#define DEC 0.5 // 감속력
+#define FRC 0.046875; // 마찰력
+#define TOPYSPEED 6 // 수직가속력 최대값
+#define SLOPE 0.125 // 슬로프 걷기/뛰기 일반값
+#define ROLLINGUP 0.078125 // 슬로프 상승 값
+#define ROLLINGDOWN 0.3125 // 슬로프 하강 값
+#define FALL 2.5; // 낙하속도
+
+//Sonic's Airborne Speed Constants ( 공중 스피드 관련값 )
+#define AIR 0.09375 // 공기저항계수
+#define JUMP 6.5 // 점프력
+#define GRAVITY 0.21875 // 중력값
+
 
 extern HWND			g_hWnd;
--- Default/Player.h
+++ Default/Player.h
@@ -12,8 +12,8 @@ public:
 
 public:
 	virtual void Initialize(void) override;
-	virtual int		Update(void) override;
-	virtual		void	Late_Update(void);
+	virtual int	Update(void) override;
+	virtual	void Late_Update(void);
 	virtual void Render(HDC hDC) override;
 	virtual void Release(void) override;
 
